afe70511ea18ea9aff680936ee5f49cfa1ccbeda
Clecio-Silva <kellciity100@gmail.com>
2026-06-15 22:00:12 -0300

    Melhorias

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0+--7+xllc_ge)evtgvbih%4nqp0ap(62ue@hc_(hapmzn&fzj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'equipamentoslab.pythonanywhere.com',
